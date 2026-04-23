eb2b51252dfc2fa16ba92655c5fba6bcd8f99528
monaabishek2003 <monaabisheka@gmail.com>
2026-04-23 20:35:05 +0000

    feat: capture agents that needs attention

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import prisma from './lib/prisma';
+import { off } from 'node:cluster';
+import { AgentScalarFieldEnum } from './generated/prisma/internal/prismaNamespace';
 
 const app = express();
 app.use(express.json());
+
+const STUCK_INTERVAL_THRESHHOLD  = 40 * 1000;
+
 
 // 1.Start an agent
 app.post('/agent/start', async (req, res) => {
@@ -75,6 +80,39 @@ app.get('/agent/:id', async (req, res) => {
   if(!agent) return res.status(404).json({error: "Not Found"});
   res.json(agent);
 });
+
+setInterval(async () => {
+  const runningAgents = await prisma.agent.findMany({
+    where: { status: "running", needsAttention: false},
+    include: { events: { orderBy: { timestamp: "desc"}, take: 1}}
+  })
+  
+
+  for(const agent of runningAgents){
+    const now = new Date().getTime();
+    const lastUpdated = new Date(agent.lastUpdateAt).getTime();
+
+    if(now - lastUpdated > STUCK_INTERVAL_THRESHHOLD){
+      const lastMessage = agent.events[0]?.message?.toLowerCase() ?? "";
+      let reason = `No update for ${Math.round(now-lastUpdated) / 1000}s`;
+
+      if (lastMessage.includes('search') || lastMessage.includes('fetch') || lastMessage.includes('request')) {
+        reason = 'Possibly waiting on API';
+      } else if (lastMessage.includes('compile') || lastMessage.includes('build')) {
+        reason = 'Build may have hung';
+      } else if (lastMessage.includes('wait') || lastMessage.includes('input') || lastMessage.includes('approval')) {
+        reason = 'Awaiting user input';
+      }
+
+      await prisma.agent.update({
+        where: { id: agent.id},
+        data : { needsAttention: true, attentionReason: reason}
+      })
+
+      console.log(`[ATTENTION} flagged agent ${agent.name}: ${reason}`);
+    }
+  }
+},5000)
 
 const port = Number(process.env.PORT) || 8000;
 app.listen(port, () => console.log(`Server on: ${port}`));
